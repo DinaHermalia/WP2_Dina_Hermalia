23cfee0271af7949e5b8b6bbbb8d1c753a284d76
DinaHermalia <dinahermalia2000@gmail.com>
2023-04-03 16:29:16 +0700

    WP2_Dina_Hermalia

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dina</li>
+        <li>Nama Belakang : Hermalia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jln. Warung Gantung KP.Kojan</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
